7f9b5cdc87edb98454178d70577baaa6984cbbc3
Axel Lin <axel.lin@gmail.com>
2012-08-06 23:44:19 +0800

    regulator: core: Fix cast to pointer from integer of different size warning
    This is to address the following warning during compilation time: (Compile on x86_64)
    
      CC      drivers/regulator/core.o
    drivers/regulator/core.c: In function '_regulator_do_set_voltage':
    drivers/regulator/core.c:2183:10: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
    
    This patch adds a temporary variable to avoid double cast.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: mourta <mourta.nova@gmail.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2324,9 +2324,12 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 		}
 	}
 
-	if (ret == 0 && best_val >= 0)
+	if (ret == 0 && best_val >= 0) {
+		unsigned long data = best_val;
+
 		_notifier_call_chain(rdev, REGULATOR_EVENT_VOLTAGE_CHANGE,
-				     (void *)best_val);
+				     (void *)data);
+	}
 
 	if (_regulator_is_enabled(rdev)) {
 		if (selector != -1)
