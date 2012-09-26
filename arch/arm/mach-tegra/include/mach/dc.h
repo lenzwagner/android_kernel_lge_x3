098e173e58dea2e1488265a2a05772b3ea403b62
Rakesh Iyer <riyer@nvidia.com>
2012-09-26 11:35:45 -0700

    video: tegra: dsi: set dsib clock source to pll_d
    Program dsib clock source to pll_d. Add mechanism for panels to be reset
    during command sequences.
    
    Change-Id: Ia426d7ae0f5f849911f1da705f9634988d874cae
    Signed-off-by: Rakesh Iyer <riyer@nvidia.com>
    Reviewed-on: http://git-master/r/139116
    (cherry picked from commit 7e95c6c88e89865de973b5a6bd2b25e5a69d27b6)
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: http://git-master/r/147610
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -89,6 +89,7 @@ struct tegra_dsi_cmd {
 	union {
 		u16 data_len;
 		u16 delay_ms;
+		unsigned gpio;
 		struct {
 			u8 data0;
 			u8 data1;
@@ -107,6 +108,12 @@ struct tegra_dsi_cmd {
 			.cmd_type = TEGRA_DSI_DELAY_MS, \
 			.sp_len_dly.delay_ms = ms, \
 			}
+
+#define DSI_GPIO_SET(rst_gpio, on)	{ \
+					.cmd_type = TEGRA_DSI_GPIO_SET, \
+					.data_id = on, \
+					.sp_len_dly.gpio = rst_gpio, \
+					}
 
 #define DSI_CMD_LONG(di, ptr)	{ \
 				.cmd_type = TEGRA_DSI_PACKET_CMD, \
