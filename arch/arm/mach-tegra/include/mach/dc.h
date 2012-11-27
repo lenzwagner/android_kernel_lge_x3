8c1f5e4e644d1956dd83fbdc39e056d05183a027
Kamal Kannan Balagopalan <kbalagopalan@nvidia.com>
2012-11-27 13:54:33 -0800

    video: tegra: dsi: Support sending frames at init
    Add support to send frames during DSI init sequence.
    
    Bug 1260089
    
    Change-Id: I28d0b87e0e551b88a51fefbc06180a5c83f4171e
    Signed-off-by: Kamal Kannan Balagopalan <kbalagopalan@nvidia.com>
    Reviewed-on: http://git-master/r/166735
    (cherry picked from commit c20fbe4008c14f15f304702dfd28495b4a1e0894)
    Reviewed-on: http://git-master/r/213369
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -6,7 +6,7 @@
  * Author:
  *	Erik Gilling <konkers@google.com>
  *
- * Copyright (C) 2010-2011 NVIDIA Corporation
+ * Copyright (c) 2011-2013, NVIDIA CORPORATION, All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -81,6 +81,7 @@ enum {
 	TEGRA_DSI_PACKET_CMD,
 	TEGRA_DSI_DELAY_MS,
 	TEGRA_DSI_GPIO_SET,
+	TEGRA_DSI_SEND_FRAME,
 };
 
 struct tegra_dsi_cmd {
@@ -90,6 +91,7 @@ struct tegra_dsi_cmd {
 		u16 data_len;
 		u16 delay_ms;
 		unsigned gpio;
+		u16 frame_cnt;
 		struct {
 			u8 data0;
 			u8 data1;
@@ -121,6 +123,11 @@ struct tegra_dsi_cmd {
 				.sp_len_dly.data_len = ARRAY_SIZE(ptr), \
 				.pdata = ptr, \
 				}
+
+#define DSI_SEND_FRAME(cnt)	{ \
+			.cmd_type = TEGRA_DSI_SEND_FRAME, \
+			.sp_len_dly.frame_cnt = cnt, \
+			}
 
 struct dsi_phy_timing_ns {
 	u16		t_hsdexit_ns;
@@ -437,6 +444,7 @@ struct tegra_dc_out {
 	int	(*postsuspend)(void);
 	int	(*bridge_reset)(void);
 	int	(*ic_reset)(void);
+	void	(*hotplug_report)(bool);
 };
 
 /* bits for tegra_dc_out.flags */
