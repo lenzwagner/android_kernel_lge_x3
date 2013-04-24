acc307b941906abfe1f971f39a74cf29743278aa
Min-wuk Lee <mlee@nvidia.com>
2013-04-24 11:31:07 +0900

    video: tegra: host: add DT support
    - this commit adds the infrastructure to parse and
    allocate device from DT
    - it also adds support to parse and add resources from DT
    into the newly allocated device
    - it also matches the device and drivers using .compatible
    property
    - of_platform_populate() assigns platform_bus as host1x parent, so
    condition in nvhost_get_parent() was modified
    
    bug 1041377
    bug 1240921
    
    Change-Id: I3373b5fb48e417e14f819d15e33f94ea1efb6194
    
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Signed-off-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-on: http://git-master/r/145937
    (cherry picked from commit 83de44b147fd9582f73fb3f1aec105c6bcebbd3e)
    Change-Id: Id53db1638ea02e7d19bee3c020c43051c74e4365
    Reviewed-on: http://git-master/r/229228
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -140,7 +140,7 @@ static void to_state_clockgated_locked(struct platform_device *dev)
 		for (i = 0; i < pdata->num_clks; i++)
 			clk_disable_unprepare(pdata->clk[i]);
 
-		if (dev->dev.parent)
+		if (nvhost_get_parent(dev))
 			nvhost_module_idle(to_platform_device(dev->dev.parent));
 
 		if (!pdata->can_powergate) {
@@ -174,7 +174,7 @@ static void to_state_running_locked(struct platform_device *dev)
 		if (!pdata->can_powergate)
 			pm_runtime_get_sync(&dev->dev);
 
-		if (dev->dev.parent)
+		if (nvhost_get_parent(dev))
 			nvhost_module_busy(to_platform_device(dev->dev.parent));
 
 		for (i = 0; i < pdata->num_clks; i++) {
@@ -748,7 +748,7 @@ bool nvhost_module_powered_ext(struct platform_device *dev)
 {
 	struct platform_device *pdev;
 
-	BUG_ON(!dev->dev.parent);
+	BUG_ON(!nvhost_get_parent(dev));
 
 	/* get the parent */
 	pdev = to_platform_device(dev->dev.parent);
@@ -760,7 +760,7 @@ void nvhost_module_busy_ext(struct platform_device *dev)
 {
 	struct platform_device *pdev;
 
-	BUG_ON(!dev->dev.parent);
+	BUG_ON(!nvhost_get_parent(dev));
 
 	/* get the parent */
 	pdev = to_platform_device(dev->dev.parent);
@@ -772,7 +772,7 @@ void nvhost_module_idle_ext(struct platform_device *dev)
 {
 	struct platform_device *pdev;
 
-	BUG_ON(!dev->dev.parent);
+	BUG_ON(!nvhost_get_parent(dev));
 
 	/* get the parent */
 	pdev = to_platform_device(dev->dev.parent);
