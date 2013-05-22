5dc1701d7be48b497b9dda37c4a87d1101e244ca
Paul Reioux <reioux@gmail.com>
2013-05-22 18:02:19 -0500

    dynamic fsync: add kernel panic notifier to force flush outstanding data
    more paranoia safety checks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -144,12 +144,29 @@ static struct notifier_block dyn_fsync_panic_block = {
 	.priority       = INT_MAX,
 };
 
+static int dyn_fsync_notify_sys(struct notifier_block *this, unsigned long code,
+				void *unused)
+{
+	if (code == SYS_DOWN || code == SYS_HALT) {
+		early_suspend_active = true;
+		dyn_fsync_force_flush();
+		//pr_warn("dyn fsync: reboot: force flush!\n");
+	}
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block dyn_fsync_notifier = {
+	.notifier_call = dyn_fsync_notify_sys,
+};
+
 static int dyn_fsync_init(void)
 {
 	int sysfs_result;
 
 	register_early_suspend(&dyn_fsync_early_suspend_handler);
 	register_reboot_notifier(&dyn_fsync_notifier);
+	atomic_notifier_chain_register(&panic_notifier_list,
+		&dyn_fsync_panic_block);
 
 	dyn_fsync_kobj = kobject_create_and_add("dyn_fsync", kernel_kobj);
 	if (!dyn_fsync_kobj) {
@@ -170,6 +187,8 @@ static void dyn_fsync_exit(void)
 {
 	unregister_early_suspend(&dyn_fsync_early_suspend_handler);
 	unregister_reboot_notifier(&dyn_fsync_notifier);
+	atomic_notifier_chain_unregister(&panic_notifier_list,
+		&dyn_fsync_panic_block);
 
 	if (dyn_fsync_kobj != NULL)
 		kobject_put(dyn_fsync_kobj);
