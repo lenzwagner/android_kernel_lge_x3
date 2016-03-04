f672eb392202209c0255e7106226722069d99fc6
mourta <mourta.nova@gmail.com>
2016-03-04 10:39:00 +0100

    Update to 3.4.107 stage 1
    Signed-off-by: mourta <mourta.nova@gmail.com>

--- ipc/util.c
+++ ipc/util.c
@@ -267,7 +267,7 @@ int ipc_addid(struct ipc_ids* ids, struct kern_ipc_perm* new, int size)
 	rcu_read_lock();
 	spin_lock(&new->lock);
 
-	id = idr_alloc(&ids->ipcs_idr, new, &id);
+	id = idr_alloc(&ids->ipcs_idr, new, id, 0, GFP_NOWAIT);
 	idr_preload_end();
 	if (id < 0) {
 		spin_unlock(&new->lock);
